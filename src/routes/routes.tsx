504c94680340b6f36b8209ad6b683d865398a7cc
Alex <alexgrigoriev.3614@gmail.com>
2024-10-13 11:04:13 +0300

    some fix

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -10,7 +10,7 @@ export const privateRoutes = [
         element: <TaskPage />,
         path: "/tasks/:id",
     },
-    { element: <Redirect />, path: "/" },
+    { element: <Redirect />, path: "*" },
 ];
 
 export const publicRoutes = [
